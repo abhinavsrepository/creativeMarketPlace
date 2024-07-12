ca559d0ad953862df9944298a927e451473c437d
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-07-12 16:12:46 +0530

    hello

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const PRODUCT_CATEGORIES = [
+{
+    label:"Vector Graphics",
+    value:"vector_graphics" as const,
+    feature:[
+        {
+            name:"Editor Top Picks",
+            href:"#",
+            imageSrc:'/nav/ui-kits/mixed.jpg'
+        }
+    ]
+}
+]
