7b1578be6c2d9b05a981bcd5ea76428a10ed1236
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-07-15 17:17:45 +0530

    initial commit

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 import path from 'path'
 import type { InitOptions } from 'payload/config'
+import payload from 'payload'
+import { init } from 'next/dist/compiled/webpack/webpack'
 dotenv.config({
     path:path.resolve(__dirname,'../.env')
 })
@@ -20,4 +22,22 @@ export const getPayloadClient = async ({initOptions,}:Args={}) => {
         throw new Error("PAYLOAD_SECRET is not defined")
     }
     
+    if(cached.client){
+        return cached.client
+    }
+    if(!cached.promise){
+        cached.promise = payload.init({
+            secret:process.env.PAYLOAD_SECRET,
+            local:initOptions?.express ? false : true,
+            ...(initOptions || {}),
+         })
+    }
+
+    try{
+        cached.client =await cached.promise
+    }catch(e:unknown){
+        cached.promise =null 
+        throw e
+    }
+    return cached.clients
 }
