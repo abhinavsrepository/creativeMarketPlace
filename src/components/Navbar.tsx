e1f66ee2c25010708f4f9f3b0110f3582f61e948
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-07-15 15:35:30 +0530

    initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./Navitems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
   const user = null;
@@ -35,7 +36,25 @@ const Navbar = () => {
                       Sign In
                     </Link>
                   )}
-                  {user ? null:(<span className="h-6 w-px bg-gray-200" aria-hidden="true" />)}
+                  {user ? null:(
+                    <span className="h-6 w-px bg-gray-200" 
+                    aria-hidden="true" />)}
+                  {user ? (<p></p>) :(
+                  <Link href='/sign-up'
+                   className={buttonVariants({variant:"ghost"})}>
+                    Create Account
+                   </Link>
+                )}
+                {user ? (<span className="h-6 w-px bg-gray-200" 
+                    aria-hidden="true" />):null}
+                    {user ? null :(<div className="flex lg:ml-6">
+                        <span className="h-6 w-px bg-gray-200" aria-hidden="true" 
+                        />
+                        </div>
+                    )}
+                <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart/>
+                </div>
                 </div>
               </div>
             </div>
